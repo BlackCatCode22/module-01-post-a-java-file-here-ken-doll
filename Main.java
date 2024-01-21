75ce9ae71bcd7c5043caa54c68826a3795b92bbe
ken-doll <157074036+ken-doll@users.noreply.github.com>
2024-01-20 21:23:22 -0800

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,13 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+        System.out.printf("Hello world!");
+        int num1 = 2;
+        int num2 = 4;
+        int sum =0;
+        sum = num1 + num2;
+        System.out.println("\n\n The sum of "+num1+" and "+num2+" equals "+sum+"\n\n");
+
+    }
+}
